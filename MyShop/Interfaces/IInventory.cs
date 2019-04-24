2b0ef991dc6cc54259086652706c79ad094fd4ea
Chris <chrismorton.cm@gmail.com>
2019-04-24 11:03:39 -0700

    added interface and added to product model

--- MyShop/Interfaces/IInventory.cs
+++ MyShop/Interfaces/IInventory.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using MyShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyShop.Interfaces
 {
-    public class IInventory
+    interface IInventory
     {
+        void CreateProduct(Product product);
+
+        void GetALLProducts();
+
+        void GetByID(int id);
+
+        void Update(int id);
+
+        void Delete(int id);
     }
 }
--- MyShop/Models/Product.cs
+++ MyShop/Models/Product.cs
@@ -12,5 +12,9 @@ namespace MyShop.Models
         public string Type { get; set; }
         public int Price { get; set; }
         public int Description { get; set; }
+        public string ImageStuffedAnimal { get; set; }
+        public string ImageAnimal { get; set; }
+        public string Summary { get; set; }
+        public string AmmountLeft { get; set; }
     }
 }
