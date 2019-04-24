cc81df887848e00c0ba17ca2dc765fa767850e1d
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-04-23 18:41:25 -0700

    initial commit

---
+++ MyShop/data/MyShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.data
+{
+    public class MyShopDbContext : DbContext
+    {
+        public MyShopDbContext(DbContextOptions<MyShopDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
