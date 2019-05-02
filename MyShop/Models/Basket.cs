f6deb4946974420515692b48c654096a1bb00ff4
Chris <chrismorton.cm@gmail.com>
2019-05-01 18:34:55 -0700

    built models for orders and basket

---
+++ MyShop/Models/Basket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.Models
+{
+    public class Basket
+    {
+        public int ID { get; set; }
+        public int TotalItems { get; set; }
+        public double TotalPrice { get; set; }
+        public string UserID { get; set; }
+        public List<BasketItems> BasketList { get; set; }
+    }
+}
---
+++ MyShop/Models/BasketItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.Models
+{
+    public class BasketItems
+    {
+        public int ID { get; set; }
+        public int BasketID { get; set; }
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double LineItemAmount { get; set; }
+    }
+}
