40595d7e706136538a433125f32a15c81d8b4412
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-05-03 17:43:08 -0700

    stashed

--- MyShop/Controllers/ShopController.cs
+++ MyShop/Controllers/ShopController.cs
@@ -4,19 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyShop.Interfaces;
+using MyShop.Models;
+using MyShop.Models.Interfaces;
 
 namespace MyShop.Controllers
 {
     public class ShopController : Controller
     {
         private IInventoryManager _context;
+        private IBasketManager _basket;
+
         /// <summary>
         /// bring in context
         /// </summary>
         /// <param name="context">database</param>
-        public ShopController(IInventoryManager context)
+        public ShopController(IInventoryManager context, IBasketManager basket)
         {
             _context = context;
+            _basket = basket;
         }
         /// <summary>
         /// the home page of shop renders all of the shop items
@@ -43,5 +48,6 @@ namespace MyShop.Controllers
             }
             return View(product);
         }
+
     }
 }
