1a13092c860a97e6d9e524b91071db5ce89d5157
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-05-18 20:35:58 -0700

    add email to profile page

--- MyShop/Pages/Profile/Profile.cshtml.cs
+++ MyShop/Pages/Profile/Profile.cshtml.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyShop.data;
+using MyShop.Models;
 
 namespace MyShop.Pages.Profile
 {
     public class ProfileModel : PageModel
     {
+        private SignInManager<ApplicationUser> _signInManager;
+        private ApplicationDbContext _context;
+
+        public ProfileModel(SignInManager<ApplicationUser> SignInManager, ApplicationDbContext context)
+        {
+            _signInManager = SignInManager;
+            _context = context;
+        }
         public void OnGet(string username)
         {
+           
+            
+            
 
         }
     }
